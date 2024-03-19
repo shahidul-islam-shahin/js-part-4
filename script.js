81f175a2fc66e8c135eb1c151a350c3c1ac1b904
shahidul-islam-shahin <shahinshahidulislam7@gmail.com>
2024-03-19 21:23:37 +0600

    js part 4

---
+++ script.js
@@ -0,0 +1,10 @@
+const firstName = 'shahidul';
+let lastName = 'islam';
+let x = 90;
+let y = 100;
+let m = 70;
+let w = 'shain';
+
+let z = x + y + w + m;
+
+document.getElementById('html').innerHTML = z;
